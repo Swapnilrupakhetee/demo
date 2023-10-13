425b8da3a0465dc94b79b0660ee6ec60684d2dcc
Swapnil <rupakheteeswapnil@gmail.com>
2023-10-13 21:20:19 +0545

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from'react';
 
 function App() {
+
+  const [numberValue, setNumberValue] =useState(0);
+  const Addition = () => {
+    setNumberValue(numberValue + 1);
+  };
+  const Minus = () => {
+    setNumberValue(numberValue - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Add and Minus butto</h1>
+      <button onClick={Addition}>Add</button>
+      <div>{numberValue}</div>
+      <button onClick={Minus}>Subtract</button>
     </div>
   );
+  
 }
 
 export default App;
